768fb560b3b749f5c85809513af2e054c8019589
Sharonwoon <sharonwoon825@gmail.com>
2024-10-17 13:32:09 +0800

    Update Task8.1.js

--- Task8.1.js
+++ Task8.1.js
@@ -17,7 +17,7 @@ function init() {
                 .attr("height",h)
                 .attr("fill","steelblue");
 
-    d3.json("https://raw.githubusercontent.com/Sharonwoon/DataVIsualisationLab/main/LGA_VIC.json").then(function(json) {
+    d3.json("https://cors-anywhere.herokuapp.com/https://raw.githubusercontent.com/Sharonwoon/DataVIsualisationLab/main/LGA_VIC.json").then(function(json) {
 
         svg.selectAll("path")
             .data(json.features)
